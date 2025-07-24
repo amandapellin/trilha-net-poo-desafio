a9222e91923d93db9983dfb370e2ec6f568b8c3d
Amanda Pellin <amandapellin@icloud.com>
2025-07-24 17:11:37 -0300

    resolução do desafio, implementando as classes e métodos faltantes.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "2245896", modelo: "Modelo A", imei: "11115556", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "88965576", modelo: "Modelo 14Plus", imei: "77788564", memoria: 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
